a4888b02cef65e033f4f108d95d6b5c7fde47eac
Bobby Signor <bsigrulz@gmail.com>
2013-06-21 09:06:12 -0800

    Adds Up navigation from large image viewer.

--- PuffinFeeder/src/main/java/edu/alaska/gina/feeder/puffinfeeder/ImageViewerActivity.java
+++ PuffinFeeder/src/main/java/edu/alaska/gina/feeder/puffinfeeder/ImageViewerActivity.java
@@ -2,21 +2,28 @@ package edu.alaska.gina.feeder.puffinfeeder;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.app.NavUtils;
 import android.util.Log;
 import android.webkit.WebView;
 import android.widget.Toast;
+
+import com.actionbarsherlock.app.SherlockActivity;
+import com.actionbarsherlock.view.MenuItem;
 
 /**
  * Takes the information passed from the main activity, converts it into a REST
  * compatable URL, then displays the content at that link (the image) in a WebView.
  * Created by bobby on 6/14/13.
  */
-public class ImageViewerActivity extends Activity {
+public class ImageViewerActivity extends SherlockActivity {
     protected String image_url;
     protected String title;
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_image_viewer);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
 
         Bundle extra = getIntent().getExtras();
         if (extra != null) {
@@ -31,5 +38,16 @@ public class ImageViewerActivity extends Activity {
 
         WebView image_frame = (WebView) findViewById(R.id.feed_image_webView);
         image_frame.loadUrl(image_url);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                NavUtils.navigateUpFromSameTask(this);
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
