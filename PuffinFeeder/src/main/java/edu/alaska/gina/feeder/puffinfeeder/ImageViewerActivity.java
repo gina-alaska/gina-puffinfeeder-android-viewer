565fb3882944107baa15b2f7c6b660a6485e59ad
Bobby Signor <bsigrulz@gmail.com>
2013-06-28 09:30:20 -0800

    ImageViewerActivity no longer restarts on screen rotation!

--- PuffinFeeder/src/main/java/edu/alaska/gina/feeder/puffinfeeder/ImageViewerActivity.java
+++ PuffinFeeder/src/main/java/edu/alaska/gina/feeder/puffinfeeder/ImageViewerActivity.java
@@ -1,5 +1,6 @@
 package edu.alaska.gina.feeder.puffinfeeder;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.v4.app.NavUtils;
 import android.util.Log;
@@ -17,6 +18,7 @@ import com.actionbarsherlock.view.MenuItem;
 public class ImageViewerActivity extends SherlockActivity {
     protected String image_url;
     protected String title;
+    protected WebView image_frame;
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,23 +29,33 @@ public class ImageViewerActivity extends SherlockActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setHomeButtonEnabled(true);
 
-        Bundle extra = getIntent().getExtras();
-        if (extra != null) {
-            image_url = extra.getString("image_url");
-            title = extra.getString("bar_title");
-        }
-        else {
-            Log.d("Puffin Feeder", "No Image URL. Please Fix that...");
-            Toast.makeText(this, "No Image URL. Please fix that...", Toast.LENGTH_SHORT).show();
-            return;
-        }
-
-        getSupportActionBar().setTitle(title);
-        WebView image_frame = (WebView) findViewById(R.id.feed_image_webView);
+        image_frame = (WebView) findViewById(R.id.feed_image_webView);
         image_frame.getSettings().setBuiltInZoomControls(true);
         image_frame.getSettings().setLoadWithOverviewMode(true);
         image_frame.getSettings().setUseWideViewPort(true);
-        image_frame.loadUrl(image_url);
+
+        if (savedInstanceState != null)
+            image_frame.restoreState(savedInstanceState);
+        else {
+            Bundle extra = getIntent().getExtras();
+            if (extra != null) {
+                image_url = extra.getString("image_url");
+                title = extra.getString("bar_title");
+            }
+            else {
+                Log.d("Puffin Feeder", "No Image URL. Please Fix that...");
+                Toast.makeText(this, "No Image URL. Please fix that...", Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            getSupportActionBar().setTitle(title);
+            image_frame.loadUrl(image_url);
+        }
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
     }
 
     @Override
