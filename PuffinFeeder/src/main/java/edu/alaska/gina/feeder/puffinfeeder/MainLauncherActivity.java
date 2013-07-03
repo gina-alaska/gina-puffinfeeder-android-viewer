6fb49a03e2050e0a5efe658edfed81bd3d0b54f9
Bobby Signor <bsigrulz@gmail.com>
2013-07-03 09:24:31 -0800

    Cleanup.

--- PuffinFeeder/src/main/java/edu/alaska/gina/feeder/puffinfeeder/MainLauncherActivity.java
+++ PuffinFeeder/src/main/java/edu/alaska/gina/feeder/puffinfeeder/MainLauncherActivity.java
@@ -13,11 +13,9 @@ import android.view.*;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.actionbarsherlock.app.*;
-import com.actionbarsherlock.view.*;
 import com.actionbarsherlock.view.Menu;
 import com.actionbarsherlock.view.MenuItem;
 import com.actionbarsherlock.view.Window;
@@ -42,9 +40,6 @@ public class MainLauncherActivity extends SherlockFragmentActivity {
     protected int current = -2;
 
     protected Menu aBarMenu;
-    protected MenuItem menuItem;
-    protected ProgressBar aBarProgress;
-    protected boolean runSpinner = false;
 
     protected DrawerLayout mDrawerLayout; //Contains the entire activity.
     protected ListView mDrawerList; //ListView of Nav Drawer.
@@ -121,7 +116,7 @@ public class MainLauncherActivity extends SherlockFragmentActivity {
         mSpiceManager.start(this.getBaseContext());
 
         if (current < 0)
-            refreshFeedsList(DurationInMillis.ONE_DAY, false);
+            refreshFeedsList(DurationInMillis.ONE_DAY);
 
         mDrawerList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
@@ -158,15 +153,7 @@ public class MainLauncherActivity extends SherlockFragmentActivity {
         outState.putInt("current", current);
     }
 
-    public void refreshFeedsList(long expiration_time, boolean runSpinnerNow) {
-        this.runSpinner = runSpinnerNow;
-
-        if (runSpinner) {
-            menuItem = aBarMenu.findItem(R.id.action_refresh);
-            menuItem.setActionView(R.layout.actionbar_progress_bar);
-            menuItem.expandActionView();
-        }
-
+    public void refreshFeedsList(long expiration_time) {
         mSpiceManager.execute(new FeedsJsonRequest(), JSON_CACHE_KEY, expiration_time, new FeedsRequestListener());
     }
 
@@ -234,7 +221,7 @@ public class MainLauncherActivity extends SherlockFragmentActivity {
 
             case R.id.action_refresh:
                 if (mDrawerLayout.isDrawerOpen(mDrawerList)) {
-                    refreshFeedsList(DurationInMillis.ALWAYS_EXPIRED, true);
+                    refreshFeedsList(DurationInMillis.ALWAYS_EXPIRED);
                     primary.notifyDataSetChanged();
                     return true;
                 }
@@ -266,11 +253,6 @@ public class MainLauncherActivity extends SherlockFragmentActivity {
     private class FeedsRequestListener implements RequestListener<Feed[]> {
         @Override
         public void onRequestSuccess(Feed[] feed) {
-            if (runSpinner) {
-                menuItem.collapseActionView();
-                menuItem.setActionView(null);
-            }
-
             masterFeedsList = feed;
 
             listItems.clear();
@@ -293,11 +275,6 @@ public class MainLauncherActivity extends SherlockFragmentActivity {
 
         @Override
         public void onRequestFailure(SpiceException e) {
-            if (runSpinner) {
-                menuItem.collapseActionView();
-                menuItem.setActionView(null);
-            }
-
             Log.d("Feeder Viewer", "Feeds list load fail! " + e.getMessage() + "\n" + e.getStackTrace());
             Toast.makeText(getApplicationContext(), "Feed list load fail!", Toast.LENGTH_SHORT).show();
         }
--- PuffinFeeder/src/main/java/edu/alaska/gina/feeder/puffinfeeder/PicassoImageAdapter.java
+++ PuffinFeeder/src/main/java/edu/alaska/gina/feeder/puffinfeeder/PicassoImageAdapter.java
@@ -6,7 +6,6 @@ import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
 
-import com.actionbarsherlock.app.SherlockFragmentActivity;
 import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
@@ -18,17 +17,11 @@ import java.util.ArrayList;
 public class PicassoImageAdapter extends BaseAdapter {
     private Context mContext;
     private ArrayList<FeedImage> mFeedImages;
-    private SherlockFragmentActivity mActivity;
-    private ArrayList<Boolean> isViewed = new ArrayList<Boolean>();
     private int maxPosition = 10;
 
-    public PicassoImageAdapter(Context c, SherlockFragmentActivity a, ArrayList<FeedImage> feedImages) {
+    public PicassoImageAdapter(Context c, ArrayList<FeedImage> feedImages) {
         this.mContext = c;
         this.mFeedImages = feedImages;
-        this.mActivity = a;
-
-        for (int i = 0; i < mFeedImages.size(); i++)
-            isViewed.add(false);
     }
 
     @Override
@@ -48,7 +41,6 @@ public class PicassoImageAdapter extends BaseAdapter {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        //checkBooleanListPopulation();
         if (position > maxPosition)
             maxPosition = position;
 
@@ -59,29 +51,9 @@ public class PicassoImageAdapter extends BaseAdapter {
 
         view.setPadding(0,0,0,0);
 
-        //if (!isViewed.get(position))
-            //mActivity.setSupportProgressBarIndeterminateVisibility(true);
-
         Picasso.with(mContext).load(R.drawable.blank_feed_item).resize(250, 250).into(view);
         Picasso.with(mContext).load(mFeedImages.get(position).getThumbnail()).resize(250, 250).centerCrop().into(view);
-        //isViewed.set(position, true);
-
-        //if (position == maxPosition)
-            //mActivity.setSupportProgressBarIndeterminateVisibility(false);
 
         return view;
-    }
-
-    private void checkBooleanListPopulation() {
-        if (isViewed.size() < mFeedImages.size())
-            for (int i = isViewed.size(); i < mFeedImages.size(); i++)
-                isViewed.add(false);
-    }
-
-    private boolean viewedPrevious(int position) {
-        for (int i = position; i >= 0; i--)
-            if (!isViewed.get(i))
-                return false;
-        return true;
     }
 }
