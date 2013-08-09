1c9c75f033c5658bf816624d6a09f67c8a86d9b4
Bobby Signor <bsigrulz@gmail.com>
2013-08-09 10:16:02 -0800

    Add developer comments to Feed, FeedImage, and FeedImageImages.

--- PuffinFeeder/src/main/java/edu/alaska/gina/feeder/puffinfeeder/FeedImage.java
+++ PuffinFeeder/src/main/java/edu/alaska/gina/feeder/puffinfeeder/FeedImage.java
@@ -10,65 +10,87 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 public class FeedImage {
     private int id;
     private String title;
-    private String updated_at;
     private String event_at;
     private String thumbnail;
-    private String image;
     private FeedImageImages previews;
 
+    /**
+     * Returns the ID number.
+     * @return ID number.
+     */
     public int getId() {
         return this.id;
     }
 
+    /**
+     * Returns the image title.
+     * @return Image title.
+     */
     public String getTitle() {
         return title;
     }
 
-    public String getUpdated_at() {
-        return updated_at;
-    }
-
+    /**
+     * Returns the URL of the thumbnail.
+     * @return Thumbnail's URL.
+     */
     public String getThumbnail() {
         return thumbnail;
     }
 
-    public String getImage() {
-        return image;
-    }
-
+    /**
+     * Returns object containing the URL's of the different sizes of the image.
+     * @return FeedImageImages object with all 3 image sizes.
+     */
     public FeedImageImages getPreviews() {
         return previews;
     }
 
-    public void setEvent_at(String event_at) {
-        this.event_at = event_at;
+    /**
+     * Returns the time the image was processed as a String (ISO 8601).
+     * @return Time image was processed (ISO 8601).
+     */
+    public String getEvent_at() {
+        return event_at;
     }
 
+    /**
+     * Sets the ID number.
+     * @param id New ID number.
+     */
     public void setId(int id) {
         this.id = id;
     }
 
+    /**
+     * Sets the title of the feed.
+     * @param title New title.
+     */
     public void setTitle(String title) {
         this.title = title;
     }
 
-    public void setUpdated_at(String updated_at) {
-        this.updated_at = updated_at;
-    }
-
+    /**
+     * Sets the URL of the thumbnail.
+     * @param thumbnail New thumbnail URL.
+     */
     public void setThumbnail(String thumbnail) {
         this.thumbnail = thumbnail;
     }
 
-    public void setImage(String image) {
-        this.image = image;
-    }
-
+    /**
+     * Sets the image's FeedImageImages object that contains all the image sizes.
+     * @param preview FeedImageImages object with all the different sizes of the image.
+     */
     public void setPreviews(FeedImageImages preview) {
         this.previews = preview;
     }
 
-    public String getEvent_at() {
-        return event_at;
+    /**
+     * Sets the time the image was processed as a String (ISO 8601).
+     * @param event_at Time image was processed (ISO 8601).
+     */
+    public void setEvent_at(String event_at) {
+        this.event_at = event_at;
     }
 }
