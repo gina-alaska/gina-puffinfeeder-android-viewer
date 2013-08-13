0737c3a3ee09914e72960d6aa22628dfc7544d38
Bobby Signor <bsigrulz@gmail.com>
2013-08-13 13:52:31 -0800

    Edit manifest and att progress bars to WebViewActivity.

--- PuffinFeeder/src/main/java/edu/alaska/gina/feeder/puffinfeeder/WebViewActivity.java
+++ PuffinFeeder/src/main/java/edu/alaska/gina/feeder/puffinfeeder/WebViewActivity.java
@@ -2,7 +2,10 @@ package edu.alaska.gina.feeder.puffinfeeder;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
+import android.view.Window;
+import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 
 public class WebViewActivity extends Activity {
@@ -10,11 +13,35 @@ public class WebViewActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_PROGRESS);
+        requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
         setContentView(R.layout.activity_web_view);
-        getActionBar().setDisplayHomeAsUpEnabled(true);
+
         Bundle url = getIntent().getExtras();
         WebView screen = (WebView) findViewById(R.id.info_web_view);
-        this.getActionBar().setTitle(url.getString("title", "Page not found - GitHub"));
+        screen.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public void onProgressChanged(WebView view, int newProgress) {
+                try {
+                    setProgress(newProgress * 100);
+                    if (newProgress >= 0)
+                        setProgressBarIndeterminate(false);
+                    if (newProgress >= 100) {
+                        setProgressBarVisibility(false);
+                        setProgressBarIndeterminateVisibility(false);
+                    }
+                } catch (NullPointerException e) {
+                    Log.d(getString(R.string.app_tag), "ProgressBar NullPointer!\n" + e.getStackTrace());
+                }
+            }
+        });
+
+        setProgressBarVisibility(true);
+        setProgressBarIndeterminate(true);
+        setProgressBarIndeterminateVisibility(true);
+
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+        getActionBar().setTitle(url.getString("title", "Page not found - GitHub"));
         screen.loadUrl(url.getString("url", "https://github.com/404"));
     }
 
