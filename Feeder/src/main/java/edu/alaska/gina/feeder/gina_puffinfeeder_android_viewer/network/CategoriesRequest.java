b601a9393d39e60299e9e0d6e251f608cafc0143
Bobby Signor <signor.bobby@gmail.com>
2014-06-12 10:21:07 -0800

    Rename from "FeedsList" to "Category"

---
+++ Feeder/src/main/java/edu/alaska/gina/feeder/gina_puffinfeeder_android_viewer/network/CategoriesRequest.java
@@ -0,0 +1,21 @@
+package edu.alaska.gina.feeder.gina_puffinfeeder_android_viewer.network;
+
+import com.octo.android.robospice.request.springandroid.SpringAndroidSpiceRequest;
+import edu.alaska.gina.feeder.gina_puffinfeeder_android_viewer.data.Category;
+
+/**
+ * Created by Bobby on 6/6/2014.
+ */
+public class CategoriesRequest extends SpringAndroidSpiceRequest<Category> {
+    private final String endpoint;
+
+    public CategoriesRequest(String endpoint) {
+        super(Category.class);
+        this.endpoint = endpoint;
+    }
+
+    @Override
+    public Category loadDataFromNetwork() throws Exception {
+        return getRestTemplate().getForObject(endpoint, Category.class);
+    }
+}
