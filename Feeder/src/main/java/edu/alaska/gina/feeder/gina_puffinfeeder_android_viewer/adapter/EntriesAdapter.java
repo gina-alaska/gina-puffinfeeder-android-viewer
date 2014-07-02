1f071cb130bc444eea234cd95ce86c9cc7edfb2b
Bobby Signor <signor.bobby@gmail.com>
2014-07-02 11:32:04 -0800

    Revert "Revert "Update thumbnail view to display time difference""
    This reverts commit 0f207b42e603245f79eee58ddb4fc9e49f3ae572.

--- Feeder/src/main/java/edu/alaska/gina/feeder/gina_puffinfeeder_android_viewer/adapter/EntriesAdapter.java
+++ Feeder/src/main/java/edu/alaska/gina/feeder/gina_puffinfeeder_android_viewer/adapter/EntriesAdapter.java
@@ -1,12 +1,22 @@
 package edu.alaska.gina.feeder.gina_puffinfeeder_android_viewer.adapter;
 
 import android.content.Context;
+import android.util.DisplayMetrics;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.GridView;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.squareup.picasso.Picasso;
+
+import org.joda.time.DateTime;
+import org.joda.time.Period;
+import org.joda.time.format.PeriodFormatter;
+import org.joda.time.format.PeriodFormatterBuilder;
+
 import edu.alaska.gina.feeder.gina_puffinfeeder_android_viewer.R;
 import edu.alaska.gina.feeder.gina_puffinfeeder_android_viewer.data.Entry;
 
@@ -19,11 +29,35 @@ import java.util.ArrayList;
 public class EntriesAdapter extends BaseAdapter {
     private final Context mContext;
     private final ArrayList<Entry> entries;
-    private int maxPosition = 10;
+    private final LayoutInflater inflater;
 
     public EntriesAdapter(Context c, ArrayList<Entry> entries) {
         this.mContext = c;
         this.entries = entries;
+        this.inflater = LayoutInflater.from(mContext);
+    }
+
+    private String formatDateTime(DateTime img) {
+        Period diff = new Period(img, new DateTime(System.currentTimeMillis()));
+        PeriodFormatter formatter;
+
+        if (diff.getYears() > 0) {
+            formatter = new PeriodFormatterBuilder().appendYears().appendSuffix(" year ago.", " years ago.").toFormatter();
+        } else if (diff.getMonths() > 0) {
+            formatter = new PeriodFormatterBuilder().appendMonths().appendSuffix(" month ago.", " months ago.").toFormatter();
+        } else if (diff.getWeeks() > 0) {
+            formatter = new PeriodFormatterBuilder().appendWeeks().appendSuffix(" week ago.", " weeks ago.").toFormatter();
+        } else if (diff.getDays() > 0) {
+            formatter = new PeriodFormatterBuilder().appendDays().appendSuffix(" day ago.", " days ago.").toFormatter();
+        } else if (diff.getHours() > 0) {
+            formatter = new PeriodFormatterBuilder().appendHours().appendSuffix(" hour ago.", " hours ago.").toFormatter();
+        } else if (diff.getMinutes() > 0) {
+            formatter = new PeriodFormatterBuilder().appendMinutes().appendSuffix(" minute ago.", " minutes ago.").toFormatter();
+        } else {
+            formatter = new PeriodFormatterBuilder().appendSeconds().appendSuffix(" second ago.", " seconds ago.").toFormatter();
+        }
+
+        return formatter.print(diff);
     }
 
     @Override
@@ -43,19 +77,25 @@ public class EntriesAdapter extends BaseAdapter {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        if (position > maxPosition)
-            maxPosition = position;
+        if (convertView == null) {
+            convertView = inflater.inflate(R.layout.entry_item_layout, null);
+        }
 
-        ImageView view = (ImageView) convertView;
+        DisplayMetrics d = mContext.getResources().getDisplayMetrics();
+        int h = Math.round(d.widthPixels / ((GridView) parent).getNumColumns());
 
-        if (view == null)
-            view = new ImageView(mContext);
+        ((TextView) convertView.findViewById(R.id.entry_caption))
+                .setText(formatDateTime(this.entries.get(position).event_at));
 
-        view.setAdjustViewBounds(true);
-        view.setPadding(0,0,0,0);
+        Picasso.with(mContext)
+                .load(entries.get(position).preview_url + "?size=" + h + "x" + (h + 50))
+                .placeholder(R.drawable.image_placeholder)
+                .resize(h, h)
+                .centerCrop()
+                .into((ImageView) convertView.findViewById(R.id.entryImage));
 
-        Picasso.with(mContext).load(entries.get(position).preview_url).placeholder(R.drawable.image_placeholder).into(view);
+        convertView.setMinimumHeight(h);
 
-        return view;
+        return convertView;
     }
 }
